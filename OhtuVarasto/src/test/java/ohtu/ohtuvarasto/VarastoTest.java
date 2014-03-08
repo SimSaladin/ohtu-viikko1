8f881b27e6b5a183f55f292a886d6693eb0f87b2
Samuli Thomasson <simsaladin@paivola.fi>
2014-03-08 23:24:21 +0200

    break VarastoTest.java

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class VarastoTest {
+ublic class VarastoTest {
 
     Varasto varasto;
     double vertailuTarkkuus = 0.0001;
