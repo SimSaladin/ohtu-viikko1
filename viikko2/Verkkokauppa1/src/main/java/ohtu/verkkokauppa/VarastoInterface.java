33cdaf73684b594ce0c2eedfee8b2722b4cef247
Samuli Thomasson <simsaladin@paivola.fi>
2014-03-23 00:17:25 +0200

    verkkokauppa no singletons

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/VarastoInterface.java
@@ -0,0 +1,12 @@
+package ohtu.verkkokauppa;
+
+interface VarastoInterface {
+
+    Tuote haeTuote(int id);
+
+    int saldo(int id);
+
+    void otaVarastosta(Tuote t);
+
+    void palautaVarastoon(Tuote t);
+}
